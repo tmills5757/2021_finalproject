69475806d356c28129dd3b4229f3e80f2009cc55
tmills5757 <78284816+tmills5757@users.noreply.github.com>
2021-04-15 19:29:19 -0500

    Added basemap and imported Leaflet libraries

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-//insert code here!
+//declare basemap variable in global scope
+var basemap;
+
+//create map
+function createMap(){
+
+    //create the map
+    basemap = L.map('basemap', {
+        center: [43.0731, -89.4012], //centered around coordinates of Madison
+        zoom: 12
+    });
+
+    //add OSM base tilelayer
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {         
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(basemap);
+
+    //call getData function
+    getData(basemap);
+};
+
+$(document).ready(createMap);
